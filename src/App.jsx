35b52d2da0dde318a292f82568b9bdc4b3d14b7d
rafibachri <rafibachri05@gmail.com>
2024-04-02 11:22:11 +0700

    update services page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import SearchPage from './pages/SearchPage';
 import OrderPage from './pages/OrderPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ServicePage from './pages/ServicePage';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path='/home' element={<Home />}></Route>
         <Route path='/search' element={<SearchPage />}></Route>
         <Route path='/order' element={<OrderPage />}></Route>
+        <Route path='/services' element={<ServicePage />}></Route>
       </Routes>
     </Router>
     </>
