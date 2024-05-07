4460e48d931acd5d40c323b4739505dc241423f6
Farhan AP <Farhanadika7@gmail.com>
2024-05-07 11:12:08 +0700

    ganti source buat gambar search

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -6,139 +6,150 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router-dom";
 
+import shop_1 from "../assets/shop_1.png";
+import shop_2 from "../assets/shop_2.png";
+import shop_3 from "../assets/shop_3.png";
+import shop_4 from "../assets/shop_4.png";
+import shop_5 from "../assets/shop_5.png";
+import shop_6 from "../assets/shop_6.png";
+import product_1 from "../assets/product_1.png";
+import product_2 from "../assets/product_2.png";
+import product_3 from "../assets/product_3.png";
+
+
 
 const shopImage = [
   {
-    img: "src/assets/shop_1.png",
+    img: shop_1,
     title: "The Classic Cut",
     distance: "0.2 km",
     rating: "4.8",
     description: "Kembali ke masa lalu di The Classic Cut. Tukang cukur kami yang berpengalaman berspesialisasi dalam potongan rambut tradisional pria, menawarkan hasil pudar yang tajam, pencukuran yang bersih, dan suasana yang bersahabat. Selamat datang!",
     products: [
       {
-        img: "src/assets/product_1.png",
+        img: product_1,
         title: "Shampoo",
         price: "100.000"
       },
       {
-        img: "src/assets/product_2.png",
+        img: product_2,
         title: "Pomade",
         price: "250.000"
       },
       {
-        img: "src/assets/product_3.png",
+        img: product_3,
         title: "Conditioner",
         price: "90.000"
       },
     ]
   },
   {
-    img: "src/assets/shop_2.png",
+    img: shop_2,
     title: "The Modern Man",
     distance: "0.2 km",
     rating: "4.9",
     description: "Mencari potongan rambut modern? The Modern Man melayani trendsetter. Kami menawarkan berbagai pilihan potongan rambut dan gaya, termasuk pembentukan janggut, tato rambut, dan layanan pewarnaan. Pesan janji temu Anda secara online untuk pengalaman yang lancar.",
     products: [
       {
-        img: "src/assets/product_1.png",
+        img: product_1,
         title: "Shampoo",
         price: "100.000"
       },
       {
-        img: "src/assets/product_2.png",
+        img: product_2,
         title: "Pomade",
         price: "250.000"
       },
       {
-        img: "src/assets/product_3.png",
+        img: product_3,
         title: "Conditioner",
         price: "90.000"
       },
     ]
   },
   {
-    img: "src/assets/shop_3.png",
+    img: shop_3,
     title: "Rusdi Ngawi Barbarshop",
     distance: "0.2 km",
     rating: "4.6",
     description: "Ayah dan anak, bersatu! Rusdi ngawi Barbarshop menyediakan lingkungan yang nyaman bagi segala usia untuk potong rambut. Tukang cukur kami sabar dan terampil, memastikan potongan sempurna untuk setiap anggota keluarga. Nikmati potong rambut gratis untuk anak di bawah 5 tahun.",
     products: [
       {
-        img: "src/assets/product_1.png",
+        img: product_1,
         title: "Shampoo",
         price: "100.000"
       },
       {
-        img: "src/assets/product_2.png",
+        img: product_2,
         title: "Pomade",
         price: "250.000"
       },
       {
-        img: "src/assets/product_3.png",
+        img: product_3,
         title: "Conditioner",
         price: "90.000"
       },
     ]
   },
   {
-    img: "src/assets/shop_4.png",
+    img: shop_4,
     title: "Salon Ania",
     distance: "0.3 km",
     rating: "4.3",
     description: "Biarkan rambut Anda menjadi bintang pertunjukan di Salon Ania. Penata rambut kami ahli dalam koreksi warna, ekstensi, dan segala hal yang berhubungan dengan kesehatan rambut. Bersantailah di salon mewah kami dan tinggalkan perasaan percaya diri dan cantik.",
     products: [
       {
-        img: "src/assets/product_1.png",
+        img: product_1,
         title: "Pomade",
       },
       {
-        img: "src/assets/product_2.png",
+        img: product_2,
         title: "Shampoo",
       },
       {
-        img: "src/assets/product_3.png",
+        img: product_3,
         title: "Conditioner",
       },
     ]
   },
   {
-    img: "src/assets/shop_5.png",
+    img: shop_5,
     title: "Nyalon Salon",
     distance: "0.8 km",
     rating: "4.2",
     description: "Acara khusus akan datang? Nyalon Salon berspesialisasi dalam pembaruan, kepang, dan gaya rambut formal yang menakjubkan. Baik itu pernikahan, prom, atau keluar malam, stylist kami akan menciptakan tampilan yang menarik perhatian. Pesan konsultasi untuk mendiskusikan gaya rambut impian Anda.",
     products: [
       {
-        img: "src/assets/product_1.png",
+        img: product_1,
         title: "Pomade",
       },
       {
-        img: "src/assets/product_2.png",
+        img: product_2,
         title: "Shampoo",
       },
       {
-        img: "src/assets/product_3.png",
+        img: product_3,
         title: "Conditioner",
       },
     ]
   },
   {
-    img: "src/assets/shop_6.png",
+    img: shop_6,
     title: "Mos Eisley",
     distance: "1.2 km",
     rating: "3.9",
     description: "Rangkul kecantikan alami Anda di Mos Eisley. Penata rambut kami berspesialisasi dalam menangani semua jenis dan tekstur rambut. Kami menawarkan berbagai perawatan dan layanan untuk mempercantik ikal, gulungan, atau gelombang alami Anda. Jadwalkan konsultasi untuk menciptakan rutinitas perawatan rambut yang dipersonalisasi.",
     products: [
       {
-        img: "src/assets/product_1.png",
+        img: product_1,
         title: "Pomade",
       },
       {
-        img: "src/assets/product_2.png",
+        img: product_2,
         title: "Shampoo",
       },
       {
-        img: "src/assets/product_3.png",
+        img: product_3,
         title: "Conditioner",
       },
     ]
