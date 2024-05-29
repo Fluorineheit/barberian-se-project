5bec1b3f628f5fdabc5295e4e029814d6bf2da5c
Farhan AP <Farhanadika7@gmail.com>
2024-05-29 17:43:37 +0700

    Alert dialog components update

--- src/components/AlertDialog.jsx
+++ src/components/AlertDialog.jsx
@@ -1,7 +1,9 @@
 // AlertDialog.jsx
 import { Dialog, DialogTitle, DialogContent, DialogContentText, Button } from '@mui/material';
 
-export default function AlertDialog({ open, handleClose, alertMessage }) {
+export default function AlertDialog({ open, handleYes, handleClose, alertMessage, isSecondButton=false, redirect=false }) {
+  
+
   return (
     <Dialog onClose={handleClose} open={open}>
       <DialogTitle>Alert</DialogTitle>
@@ -10,7 +12,10 @@ export default function AlertDialog({ open, handleClose, alertMessage }) {
           {alertMessage}
         </DialogContentText>
       </DialogContent>
-      <Button onClick={handleClose}>Okay</Button>
+      <div style={{padding: '10px', display: 'flex', flexDirection:'column'}}>
+        <Button style={{color: 'white', backgroundColor: '#797EF6'}} onClick={redirect ? handleYes : handleClose}>Yes</Button>
+        {isSecondButton && <Button className='text-[#797EF6]' onClick={handleClose}>No</Button>}
+      </div>
     </Dialog>
   );
 };
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,27 @@
 import NavBar from "../components/NavBar"
-import { Person, Lock, Favorite, Comment, Receipt, SupportAgent, Help } from "@mui/icons-material/";
+import { Person, Lock, Favorite, Comment, Receipt, SupportAgent, Help, Logout } from "@mui/icons-material/";
 import { Button } from "@mui/material/";
 import Stack from '@mui/material/Stack';
+import AlertDialog from "../components/AlertDialog";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
 
 
 const profileImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Yvette_performing_in_Hamden%2C_CT_-_2022.jpg/1024px-Yvette_performing_in_Hamden%2C_CT_-_2022.jpg"
 
 export default function Profile() {
+    const [open, setOpen] = useState(false);
+
+    const navigate = useNavigate()
+
+    const handleYes = () => {
+        navigate('/')
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
     return (
         <div className="container pb-2">
         <div className="flex flex-row items-center">
@@ -21,16 +36,15 @@ export default function Profile() {
             </div>
             <Stack spacing={2}>
                 <Button startIcon={<Person />} variant="text" className="justify-start rounded-xl text-black normal-case" >Manage Account</Button>
-                <Button startIcon={<Lock />} variant="text" className="justify-start rounded-xl text-black normal-case" >Privacy</Button>
                 <Button startIcon={<Favorite />} variant="text" className="justify-start rounded-xl text-black normal-case" >Your Favorite</Button>
                 <Button startIcon={<Comment />} variant="text" className="justify-start rounded-xl text-black normal-case" >Your Review</Button>
                 <Button startIcon={<Receipt />} variant="text" className="justify-start rounded-xl text-black normal-case" >Order History</Button>
-                <Button startIcon={<SupportAgent />} variant="text" className="justify-start rounded-xl text-black normal-case" >Chat Live</Button>
                 <Button startIcon={<Help />} variant="text" className="justify-start rounded-xl text-black normal-case" >Help Center</Button>
+                <Button onClick={()=>setOpen(true)} startIcon={<Logout />} variant="text" className="justify-start rounded-xl text-red-500 normal-case" >Logout</Button>
             </Stack>
             {/* flex flex-col gap-4 mt-4 justify-start */}
         </div>
-
+        <AlertDialog handleClose={handleClose} handleYes={handleYes} alertMessage={"Are you sure, you want to logout?"} open={open} isSecondButton={true} redirect={true}/>
         <NavBar />
       </div>
     )
